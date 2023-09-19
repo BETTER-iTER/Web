a25961391701d284b693579cf0e01af0067d9596
alreadynyeong <kjw87130@naver.com>
2023-09-20 03:39:33 +0900

    #1 Feat: Stitches 설치 및 설정 파일 생성

---
+++ iTER/stitches.config.ts
@@ -0,0 +1,40 @@
+import { createStitches } from "@stitches/react";
+import type * as Stitches from "@stitches/react";
+
+export const { styled, css, globalCss, keyframes, getCssText, theme } =
+  createStitches({
+    theme: {
+      colors: {
+        White: "#fff",
+        Brand: "#8787f4",
+        Black: "#242424",
+
+        ErrorRed: "#f34f45",
+
+        Gray10: "#d8dbe2",
+        Gray20: "#c1c4cc",
+        Gray30: "#8e9198",
+        Gray40: "#535659",
+        Gray50: "#36383a",
+      },
+    },
+    utils: {
+      bodyText: (level: 1 | 2) => {
+        if (level === 1) {
+          return {
+            fontSize: "15px",
+            fontWeight: "400",
+          };
+        }
+        return {
+          fontSize: "14px",
+          fontWeight: "bold",
+        };
+      },
+    },
+    media: {
+      bp1: "(min-width: 390px)",
+      bp2: "(min-width: 768px)",
+      bp3: "(min-width: 1024px)",
+    },
+  });
