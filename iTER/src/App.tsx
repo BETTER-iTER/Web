5145cbb3b36f10e177c3afa5e7c13418e337bcbf
kimtaeyoung <trust00005@naver.com>
2023-09-23 13:09:22 +0900

    #8 Feat 버튼 컴포넌트 생성

--- iTER/src/App.tsx
+++ iTER/src/App.tsx
@@ -7,6 +7,7 @@ import {
   Headline3,
   Headline4,
 } from "./component/Font";
+import Button from "./component/common/Button";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Body2>Body2</Body2>
       <Caption>Caption</Caption>
       <ButtonText>Button</ButtonText>
+      <Button disabled={false}>버튼</Button>
     </>
   );
 }
