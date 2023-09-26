b93befdae626665f716bcf33ff0751e6a5d0a26a
alreadynyeong <kjw87130@naver.com>
2023-09-26 11:43:16 +0900

    #12 Feat: 모달 컴포넌트 생성

--- iTER/src/App.tsx
+++ iTER/src/App.tsx
@@ -1,40 +1,22 @@
-import { styled } from "../stitches.config";
-import {
-  ButtonText,
-  Caption,
-  Headline1,
-  Headline2,
-  Headline3,
-  Headline4,
-} from "./component/Font";
-import Button from "./component/common/Button";
+import Modal from './component/common/Modal';
+import { useState } from 'react';
 
 function App() {
+  const [modal, setModal] = useState(false);
+
   return (
     <>
-      <Headline1>Headline 1</Headline1>
-      <Headline2>Headline2</Headline2>
-      <Headline3>Headline3</Headline3>
-      <Headline4>Headline4</Headline4>
-      <Body1>Body1</Body1>
-      <Body2>Body2</Body2>
-      <Caption>Caption</Caption>
-      <ButtonText>Button</ButtonText>
-      <Button onClick={handleClick} disabled={true}>버튼</Button>
+      <div onClick={() => setModal(true)}>MODAL</div>
+      {modal && (
+        <Modal
+          text="모달 테스트입니다."
+          onClick={() => {
+            setModal(false);
+          }}
+        />
+      )}
     </>
   );
 }
 
-const Body1 = styled("div", {
-  bodyText: 1,
-  color: "$ErrorRed",
-});
-
-const Body2 = styled("div", {
-  bodyText: 2,
-  color: "$Brand",
-});
-function handleClick() {
-  console.log("활성화된상태");
-}
 export default App;
---
+++ iTER/src/component/common/Modal.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+import { styled } from '../../../stitches.config';
+import { ButtonText, Headline4 } from '../Font';
+
+interface ModalProps {
+  text: string;
+  onClick: () => void;
+}
+
+const Modal: FC<ModalProps> = ({ text, onClick }) => {
+  return (
+    <Back>
+      <ModalBox>
+        <div style={{ height: 110 }}>
+          <Headline4>{text}</Headline4>
+        </div>
+        <Button onClick={onClick}>
+          <ButtonText>확인</ButtonText>
+        </Button>
+      </ModalBox>
+    </Back>
+  );
+};
+
+export default Modal;
+
+const Back = styled('div', {
+  backgroundColor: 'rgba(25, 25, 25, 0.8)',
+  width: '100vw',
+  height: '100vh',
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const ModalBox = styled('div', {
+  width: '340px',
+  backgroundColor: `$White`,
+  borderRadius: '20px',
+  flexDirection: 'column',
+  ' & > div': {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const Button = styled('div', {
+  borderTop: '1px solid $Gray10',
+  height: '50px',
+  cursor: 'pointer',
+});
