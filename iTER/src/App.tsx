50a41f2717ec1aca7ada0934a5a8f0c55dcc4be7
kimtaeyoung <trust00005@naver.com>
2023-10-01 20:49:13 +0900

    #18 feat input 컴포넌트 3차 수정

--- iTER/src/App.tsx
+++ iTER/src/App.tsx
@@ -2,13 +2,38 @@
 import ButtonWithInput from './component/common/Input';
 
 function App() {
+
+  //이건 그냥 버튼 활성화되는지 안되는지 확인하려고 쓴 함수
+  const handleButtonClick = () => {
+    console.log('버튼이 클릭되었습니다.');
+  };
+
+  //이메일 유효성 검사
+  const validateEmail = (value: string) => {
+    const isEmailValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+      value.trim()
+    );
+    return isEmailValid ? undefined : '유효한 이메일을 입력하세요.';
+  };
+
+
+
+
   return (
     // <Routes>
     //   <Route path="/" element={<Home />} />
     //   <Route path="/test" element={<Test />} />
       
     // </Routes>
-    <ButtonWithInput placeholder='이메일을 입력해주세요' labelName="이메일" btnName='인증번호 전송' onClick={()=> {console.log("버튼 누름")}}/>
+    
+
+    <ButtonWithInput
+        labelName="이메일"
+        btnName="확인"
+        placeholder="이메일을 입력하세요"
+        onClick={handleButtonClick}
+        onValidate={validateEmail} // 이메일 유효성 검사 함수를 prop으로 전달
+      />
   );
 }
 
--- iTER/src/component/common/Input.tsx
+++ iTER/src/component/common/Input.tsx
@@ -1,23 +1,70 @@
 import React, { useState } from 'react';
 import { styled } from '../../../stitches.config';
 
-// 인풋 컴포넌트
-interface InputProps {
-    placeholder: string;
-    onChange: (value: string) => void;
-  }
+interface ButtonWithInputProps {
+  labelName: string;
+  btnName: string;
+  placeholder: string;
+  onClick: () => void;
+  onValidate: (value: string) => string | undefined; 
+}
 
-const InputComponent: React.FC<InputProps> = ({ placeholder, onChange }) => {
-    const [inputValue, setInputValue] = useState<string>('');
-  
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const newValue = event.target.value;
-      setInputValue(newValue);
-      onChange(newValue);
-    };
-    
+const ButtonWithInput: React.FC<ButtonWithInputProps> = ({
+  labelName,
+  btnName,
+  placeholder,
+  onClick,
+  onValidate,
+}) => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [error, setError] = useState<string | undefined>('');
 
-    return (
+  const handleInputValueChange = (value: string) => {
+    setInputValue(value);
+    const validationError = onValidate(value);
+    setError(validationError);
+  };
+
+  const isInputValid = !error;
+
+  return (
+    <div>
+      <Label>{labelName}</Label>
+      <Body>
+        <InBody>
+          <InputComponent
+            placeholder={placeholder}
+            onChange={handleInputValueChange}
+          />
+          <Button
+            style={{
+              backgroundColor: isInputValid ? "#4C4E55" : "#c1c4cc",
+              pointerEvents: isInputValid ? "auto" : "none",
+            }}
+            onClick={isInputValid ? onClick : undefined}
+          >
+            {btnName}
+          </Button>
+        </InBody>
+      </Body>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </div>
+  );
+};
+
+const InputComponent: React.FC<{
+  placeholder: string;
+  onChange: (value: string) => void;
+}> = ({ placeholder, onChange }) => {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
     <>
       <input
         type="text"
@@ -29,93 +76,63 @@ const InputComponent: React.FC<InputProps> = ({ placeholder, onChange }) => {
           height: "22px",
           marginLeft: "10px",
           backgroundColor: "White",
-          outline: "none", 
-          color: "black", 
+          outline: "none",
+          color: "black",
           fontWeight: "400",
           fontSize: "14px",
           width: "200px",
         }}
       />
-
-      </>
-    );
-  };
-  
-
-  interface ButtonWithInputProps {
-    labelName: string;
-    btnName: string; // 버튼 텍스트를 받을 prop 추가
-    placeholder: string;
-    onClick: () => void; // 클릭 이벤트 핸들러를 받을 prop 추가
-  }
-  
-  
-  const ButtonWithInput: React.FC<ButtonWithInputProps> = ({ labelName, btnName, placeholder, onClick }) => {
-    const [inputValue, setInputValue] = useState<string>('');
-  
-    const handleInputValueChange = (value: string) => {
-      setInputValue(value);
-    };
-    const isInputNotEmpty = inputValue.trim() !== ''; // 입력값이 비어 있지 않으면 true
-  
-    return (
-      <>
-      <Label>{labelName}</Label>
-      <Body>
-        <InBody>
-          <InputComponent
-            placeholder={placeholder}
-            onChange={handleInputValueChange}
-          />
-          <Button style={{backgroundColor: isInputNotEmpty ? "#4C4E55" : "#c1c4cc", 
-                          pointerEvents: isInputNotEmpty ? "" : "none",
-                          }} onClick={onClick}>{btnName}</Button> 
-        </InBody>
-      </Body>
-      </>
-    );
-  };
-
+    </>
+  );
+};
 
 const Body = styled("div", {
-    border: "1px solid $Gray10",
-    borderRadius: "7px",
-    width: "340px",
-    height: "50px",
-    alignItems: "center",
-    display: "flex",
-    backgroundColor: "$White",
-    color: "black",
-  });
+  // border: "1px solid $Gray10",
+  borderRadius: "7px",
+  width: "340px",
+  height: "50px",
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "column",
+  backgroundColor: "$White",
+  color: "black",
+});
 
 const Button = styled("button", {
-    
-    color: "$White",
-    bodyText: 2,
-    height: "30px",
-    borderRadius: "5px",
-    float: "right",
-    marginRight: "10px",
-    border: "none",
-    fontSize: "14px",
-    fontWeight: "400",
-    padding: "5px 10px",
-})
+  color: "$White",
+  bodyText: 2,
+  height: "30px",
+  borderRadius: "5px",
+  margin: "10px 0",
+  border: "none",
+  fontSize: "14px",
+  fontWeight: "400",
+  padding: "5px 10px",
+});
+
 const InBody = styled("div", {
-    width: "100%",
-    height: '100%', 
-    display: 'flex', 
-    alignItems: 'center',
-    justifyContent: 'space-between',
-})
+  width: "100%",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingRight: "10px",
+});
+
 const Label = styled("div", {
   fontWeight: "600",
   fontSize: "16px",
   lineHeight: "22px",
   color: "black",
   marginBottom: "10px",
+});
 
-})
-
+const ErrorMessage = styled("div", {
+  color: "red",
+  fontSize: "13px",
+  lineHeight: "18.2px",
+  fontWeight: "400",
+  marginTop: "10px",
+});
 
 export default ButtonWithInput;
