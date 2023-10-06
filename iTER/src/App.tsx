da3386fca036c6ff8c3085750bcb73df80bd4806
alreadynyeong <kjw87130@naver.com>
2023-10-06 12:43:30 +0900

    #25 Feat: 회원가입 추가정보 페이지 생성

--- iTER/src/App.tsx
+++ iTER/src/App.tsx
@@ -2,6 +2,8 @@ import Login from './pages/Login';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/signup/SignUp';
+import SignupComplete from './pages/signup/SignupComplete';
+import SignupAdditional from './pages/signup/SignupAdditional';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/signup/additional" element={<SignupAdditional />} />
+      <Route path="/signup/complete" element={<SignupComplete />} />
     </Routes>
   );
 }
