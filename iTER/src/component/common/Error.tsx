467849e50dc32b26ef31e68d2743992676e1aff6
alreadynyeong <kjw87130@naver.com>
2023-10-06 14:42:14 +0900

    #27 Feat: 에러 화면 구현

---
+++ iTER/src/component/common/Error.tsx
@@ -0,0 +1,23 @@
+import { styled } from '../../../stitches.config';
+import ErrorNetwork from '../../assets/icon/ErrorNetwork.svg?react';
+import ErrorIcon from '../../assets/icon/ErrorIcon.svg?react';
+const ErrorPage = ({ type }: { type: number }) => {
+  return (
+    <Container>
+      {type == 1 ? <ErrorNetwork /> : <ErrorIcon width={50} height={50} fill={'black'} />}
+      {type == 1 ? '네크워크 다시 연결하기' : '다시 시도하기'}
+    </Container>
+  );
+};
+export default ErrorPage;
+
+const Container = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100vh',
+  color: '$Gray50',
+  flexDirection: 'column',
+  gap: '26px',
+});
