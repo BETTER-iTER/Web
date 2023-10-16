048e4a8d1ce37a83f3fc780d4ebcab88fe808a5a
alreadynyeong <kjw87130@naver.com>
2023-10-16 22:39:39 +0900

    #39 Feat: Nav

---
+++ iTER/src/component/layout/Nav.tsx
@@ -0,0 +1,56 @@
+import { styled } from '../../../stitches.config';
+import Home from '../../assets/icon/Home.svg?react';
+import Search from '../../assets/icon/Search.svg?react';
+import Write from '../../assets/icon/Write.svg?react';
+import Mypage from '../../assets/icon/Mypage.svg?react';
+import NavRouterList from '../../constants/NavRouter';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const Nav = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
+  console.log(typeof pathname);
+
+  const color = (path: string) => {
+    if (pathname === path) {
+      return '#24292F';
+    } else {
+      return '#AFB8C1';
+    }
+  };
+  return (
+    <Container>
+      {NavRouterList.map((item, index) => {
+        const { name, path } = item;
+        return (
+          <div
+            key={index}
+            onClick={() => {
+              navigate(path);
+            }}
+          >
+            {name === 'home' && <Home fill={color(path)} />}
+            {name === 'search' && <Search fill={color(path)} />}
+            {name === 'write' && <Write fill={color(path)} />}
+            {name === 'mypage' && <Mypage fill={color(path)} />}
+          </div>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default Nav;
+
+const Container = styled('div', {
+  height: '42px',
+  backgroundColor: '$White',
+  display: 'flex',
+  alignItems: 'center',
+  padding: '0 44px',
+  gap: '63px',
+  position: 'fixed',
+  bottom: '0',
+  zIndex: '1',
+});
--- iTER/src/pages/Home.tsx
+++ iTER/src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import { styled } from '@stitches/react';
+import Nav from '../component/layout/Nav';
+
 const Home = () => {
   return (
-    <div>
-      <a href="/test">테스트</a>
-    </div>
+    <Container>
+      <Nav />
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled('div', {
+  width: '390px',
+  height: '100vh',
+  backgroundColor: '$Gray10',
+  // border: '1px solid red',
+});
