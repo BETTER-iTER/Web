847a9878d9fb6149bad7fff892f14a4ce108e584
alreadynyeong <kjw87130@naver.com>
2023-10-18 10:33:39 +0900

    #43 Feat: Top 컴포넌트에 검색 추가

--- iTER/src/component/layout/Top.tsx
+++ iTER/src/component/layout/Top.tsx
@@ -3,7 +3,7 @@ import Back from '../../assets/icon/Back.svg?react';
 import { useNavigate } from 'react-router-dom';
 import { LabelText } from '../Font';
 
-const Top = ({ title, back }: { title: string; back?: () => void }) => {
+const Top = ({ title, back, search }: { title?: string; back?: () => void; search?: boolean }) => {
   const navigate = useNavigate();
 
   return (
@@ -11,9 +11,14 @@ const Top = ({ title, back }: { title: string; back?: () => void }) => {
       <BackBox onClick={back ? back : () => navigate(-1)}>
         <Back />
       </BackBox>
-      <Title>
-        <LabelText>{title}</LabelText>
-      </Title>
+      <>
+        {title && (
+          <Title>
+            <LabelText>{title}</LabelText>
+          </Title>
+        )}
+        {search && <Input placeholder="검색어를 입력해 주세요" />}
+      </>
     </Container>
   );
 };
@@ -21,24 +26,45 @@ const Top = ({ title, back }: { title: string; back?: () => void }) => {
 export default Top;
 
 const Container = styled('div', {
-  width: '100%',
+  width: '350px',
   height: '60px',
   backgroundColor: '$White',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   borderBottom: 'solid 1px #EAEEF2',
-  position: 'absolute',
-  top: '0',
-  left: '0',
-});
-
-const Title = styled('div', {
-  color: '#24292F',
+  color: '$TitleBlack',
+  padding: '0 20px',
 });
 
 const BackBox = styled('div', {
-  position: 'absolute',
-  left: '20px',
-  top: '20px',
+  width: '20px',
+  height: '20px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginRight: 'auto',
+  cursor: 'pointer',
+});
+
+const Input = styled('input', {
+  border: '1px solid $Gray10',
+  borderRadius: '5px',
+  width: '298px',
+  height: '18px',
+  padding: '10px',
+  bodyText: 2,
+  '&:focus': {
+    outline: 'none',
+  },
+  '::placeholder': {
+    color: '$Gray10',
+  },
+});
+
+const Title = styled('div', {
+  flex: 1,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 });
