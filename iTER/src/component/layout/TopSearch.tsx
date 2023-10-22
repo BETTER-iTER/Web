e5460dd092e18d864197bd403c8d1998ab24f5d7
alreadynyeong <kjw87130@naver.com>
2023-10-22 15:54:41 +0900

    #43 Fix: 추천검색어 오류 수정 및 파일 분리

--- iTER/src/component/layout/TopSearch.tsx
+++ iTER/src/component/layout/TopSearch.tsx
@@ -2,12 +2,12 @@ import { styled } from '../../../stitches.config';
 import Back from '../../assets/icon/Back.svg?react';
 import XbtnCircle from '../../assets/icon/XbtnCircle.svg?react';
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import RecommendItem from '../search/RecommendList';
 
 const TopSearch = ({
   back,
   onHandle,
-  onChange,
   searchText,
 }: {
   back?: () => void;
@@ -17,10 +17,7 @@ const TopSearch = ({
 }) => {
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState<string>(searchText || '');
-
-  useEffect(() => {
-    setSearchValue(searchText || '');
-  }, [searchText]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -29,6 +26,20 @@ const TopSearch = ({
     }
   };
 
+  const handleSelectSuggestions = (text: string) => {
+    setSearchValue(text);
+    setShowSuggestions(false);
+  };
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const text = event.target.value;
+    setSearchValue(text);
+    if (text.trim() === '') {
+      setShowSuggestions(false);
+    } else {
+      setShowSuggestions(true);
+    }
+  };
   return (
     <>
       <Container>
@@ -40,18 +51,31 @@ const TopSearch = ({
             placeholder="검색어를 입력해 주세요"
             value={searchValue}
             onChange={(event) => {
-              setSearchValue(event.target.value);
-              onChange && onChange(event);
+              handleSearch(event);
             }}
             onKeyPress={handleKeyPress}
           />
           {searchValue && (
-            <Xbox onClick={() => setSearchValue('')}>
+            <Xbox
+              onClick={() => {
+                setSearchValue('');
+                setShowSuggestions(false);
+              }}
+            >
               <XbtnCircle />
             </Xbox>
           )}
         </InputBox>
       </Container>
+      {showSuggestions && searchValue?.length > 0 && (
+        <RecommendBox>
+          {recommend
+            .filter((text) => text.includes(searchValue))
+            .map((text) => (
+              <RecommendItem key={text} text={text} onSelect={handleSelectSuggestions} />
+            ))}
+        </RecommendBox>
+      )}
     </>
   );
 };
@@ -111,3 +135,14 @@ const Xbox = styled('div', {
   justifyContent: 'center',
   cursor: 'pointer',
 });
+
+const RecommendBox = styled('div', {
+  width: '390px',
+  height: '100vh',
+  marginTop: '60px',
+  backgroundColor: '$White',
+  position: 'absolute',
+  zIndex: 1,
+});
+
+const recommend = ['추천 검색어', '추천할까', '추천할게'];
--- iTER/src/pages/Search.tsx
+++ iTER/src/pages/Search.tsx
@@ -1,18 +1,13 @@
 import { styled } from '../../stitches.config';
 import Nav from '../component/layout/Nav';
 import { useEffect, useState } from 'react';
-import Result from '../component/search/Result';
 import SearchCategory from '../component/search/Category';
-import RecommendItem from '../component/search/RecommendList';
 import TopSearch from '../component/layout/TopSearch';
 
 const Search = () => {
   const [keywords, setKeywords] = useState<{ id: number; text: string }[]>(
     JSON.parse(localStorage.getItem('keywords') || '[]')
   );
-  const [searchInputValue, setSearchInputValue] = useState('');
-  const [showSuggestions, setShowSuggestions] = useState(false);
-
   // 최근 검색어
   useEffect(() => {
     const currentDate = Date.now();
@@ -33,42 +28,13 @@ const Search = () => {
       text: text,
     };
     setKeywords([newKeyword, ...keywords]);
-    setShowSuggestions(false);
-  };
-
-  // 추천 검색어
-
-  // 값을 입력하기 시작했을 때
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const text = event.target.value;
-    setSearchInputValue(text);
-    if (text.trim() === '') {
-      setShowSuggestions(false);
-    } else {
-      setShowSuggestions(true);
-    }
-  };
-
-  const handleSelectSuggestions = (text: string) => {
-    console.log(text);
-    setSearchInputValue(text);
-    setShowSuggestions(false);
   };
 
   return (
     <Container>
-      <TopSearch onHandle={handleAdd} onChange={handleSearch} searchText={searchInputValue} />
+      <TopSearch onHandle={handleAdd} />
       {/* <Result /> */}
       <SearchCategory keywords={keywords} onDelete={handleDelete} />
-      {showSuggestions && (
-        <RecommendBox>
-          {recommend
-            .filter((text) => text.includes(searchInputValue))
-            .map((text) => (
-              <RecommendItem key={text} text={text} onSelect={handleSelectSuggestions} />
-            ))}
-        </RecommendBox>
-      )}
 
       <Nav />
     </Container>
@@ -83,14 +49,3 @@ const Container = styled('div', {
   flexDirection: 'column',
   alignItems: 'center',
 });
-
-const RecommendBox = styled('div', {
-  width: '390px',
-  height: '100vh',
-  marginTop: '60px',
-  backgroundColor: '$White',
-  position: 'absolute',
-  zIndex: 1,
-});
-
-const recommend = ['추천 검색어', '추천할까', '추천할게'];
