0bd08f88ebe1325efaf50fdc9fc6e820db6029c5
alreadynyeong <kjw87130@naver.com>
2023-10-25 15:42:00 +0900

    #29 Fix: baseURL 변경

--- iTER/src/apis/index.ts
+++ iTER/src/apis/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dev.betteritem.store',
+  // baseURL: 'https://dev.betteritem.store',
+  baseURL: 'http://13.124.170.30:8080',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
