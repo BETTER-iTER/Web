7c54e6e52ad06d99403034a3dec6c2eb1e86d62f
kimtaeyoung <trust00005@naver.com>
2023-10-29 18:21:40 +0900

    feat#50 온보딩 화면 구현

---
+++ iTER/src/pages/Onboading.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import iTER from "../assets/icon/iTER.svg";
+import { styled,css,keyframes } from '../../stitches.config';
+
+const Cover = styled("div", {
+    marginTop: "376px",
+  })
+
+  const Title = styled("div", {
+    fontSize: "30px",
+    fontWeight: "600",
+    color: "$Brand",
+}
+)
+const Box = styled("div", {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "$White",
+    textAlign: "center",
+    
+  });
+
+const Top = styled("div", {
+    fontSize: "20px",
+    fontWeight: "600",
+    color: "$Brand",
+    margin
+})
+const Onboading = () => {
+
+    return (
+    <Box>
+    <Top>
+        삶의 질을 높여주는<br/>
+        IT 제품리뷰 서비스
+    </Top>
+    <Cover>
+      <img style={{width: "100px", height: "68.7px" }} src={iTER} alt="logo"/>
+      <Title>
+        ITer
+      </Title>
+    </Cover>
+    </Box>
+    );
+};
+
+export default Onboading;
