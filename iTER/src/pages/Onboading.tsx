bdfbdfddfc7230b0d2d7c4e0f2bf065a58038dab
kimtaeyoung <trust00005@naver.com>
2023-10-30 17:21:48 +0900

    feat#50 온보딩 화면 구현

--- iTER/src/pages/Onboading.tsx
+++ iTER/src/pages/Onboading.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import iTER from "../assets/icon/iTER.svg";
 import { styled,css,keyframes } from '../../stitches.config';
-
+import { Caption1 } from '../component/Font';
+import Button from '../component/common/Button';
 const Cover = styled("div", {
-    marginTop: "376px",
+    marginTop: "213px",
   })
 
   const Title = styled("div", {
@@ -27,20 +28,39 @@ const Top = styled("div", {
     fontSize: "20px",
     fontWeight: "600",
     color: "$Brand",
+    lineHeight: "28px",
+    marginBottom: "116px",
+})
+
+const Sub = styled("div", {
+  marginTop: "145px",
+  color: "$Gray50",
+})
+
+const BtnBody = styled("div", {
+  marginTop: "34px",
 })
 const Onboading = () => {
 
     return (
     <Box>
-    <Top>
-        삶의 질을 높여주는<br/>
-        IT 제품리뷰 서비스
-    </Top>
     <Cover>
+      <Top>
+          삶의 질을 높여주는<br/>
+          IT 제품리뷰 서비스
+      </Top>
       <img style={{width: "100px", height: "68.7px" }} src={iTER} alt="logo"/>
       <Title>
         ITer
       </Title>
+      <Sub>
+        <Caption1>
+          로그인 후 더 편리해진 ITer를 이용해보세요!
+        </Caption1>
+      </Sub>
+      <BtnBody>
+        <Button onClick={()=>{window.location.href="/login"}} children="로그인" disabled={false} />
+      </BtnBody>
     </Cover>
     </Box>
     );
