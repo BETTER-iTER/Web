9528683afe4c430699f827b324d5d992687aeb7c
kimtaeyoung <trust00005@naver.com>
2023-11-04 23:58:47 +0900

    feat#64 모달창 추가

--- iTER/src/pages/user/DeleteUser.tsx
+++ iTER/src/pages/user/DeleteUser.tsx
@@ -3,9 +3,35 @@ import { B2, Headline3, Caption2 } from "../../component/Font";
 import Top from "../../component/layout/Top";
 import { styled } from "../../../stitches.config";
 import CheckCircle from "../../assets/icon/CheckCircle.svg?react";
+import Button from "../../component/common/Button";
+import Modal from "../../component/common/Modal";
 
+type SelectedOptions = {
+    [key: string]: boolean;
+  };
+  
 const DeleteUser = () => {
-    const [check, setCheck] = useState<boolean>(false);
+    // const [check, setCheck] = useState<boolean>(false);
+    const [selectedOptions, setSelectedOptions] = useState<SelectedOptions>({});
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOptionClick = (optionKey: string) => {
+        setSelectedOptions((prevSelectedOptions) => {
+          const updatedSelectedOptions = { ...prevSelectedOptions };
+          updatedSelectedOptions[optionKey] = !updatedSelectedOptions[optionKey];
+          return updatedSelectedOptions;
+        });
+      };
+
+      const isButtonEnabled = Object.values(selectedOptions).filter((value) => value).length >= 1 ;
+    
+      const openModal = () => {
+        setIsModalOpen(true);
+      };
+    
+      const closeModal = () => {
+        setIsModalOpen(false);
+      };
 
     return (
         <>
@@ -20,40 +46,50 @@ const DeleteUser = () => {
                     <B2>탈퇴하려는 이유를 모두 선택해주세요. <br />제품 개선에 활용하겠습니다.</B2>
                 </Mini2>
 
-                <Terms onClick={() => setCheck(!check)} check={check}>
-                    <CheckCircle fill={check ? '#8787F4' : '#C1C4CC'} />
-                    <Caption2>탈퇴하고 다시 가입할래요</Caption2>
-                </Terms>
+                <Options>
+                    <Terms onClick={() => handleOptionClick('option1')} check={selectedOptions['option1']}>
+                        <CheckCircle fill={selectedOptions['option1']? '#8787F4' : '#C1C4CC'} />
+                        <Caption2>탈퇴하고 다시 가입할래요</Caption2>
+                    </Terms>
 
-                <Terms onClick={() => setCheck(!check)} check={check}>
-                    <CheckCircle fill={check ? '#8787F4' : '#C1C4CC'} />
-                    <Caption2>제가 찾는 제품이 없어요</Caption2>
-                </Terms>
+                    <Terms onClick={() => handleOptionClick('option2')} check={selectedOptions['option2']}>
+                        <CheckCircle fill={selectedOptions['option2'] ? '#8787F4' : '#C1C4CC'} />
+                        <Caption2>제가 찾는 제품이 없어요</Caption2>
+                    </Terms>
 
-                <Terms onClick={() => setCheck(!check)} check={check}>
-                    <CheckCircle fill={check ? '#8787F4' : '#C1C4CC'} />
-                    <Caption2>ITer를 자주 사용하지 않아요</Caption2>
-                </Terms>
+                    <Terms onClick={() => handleOptionClick('option3')} check={selectedOptions['option3']}>
+                        <CheckCircle fill={selectedOptions['option3'] ? '#8787F4' : '#C1C4CC'} />
+                        <Caption2>ITer를 자주 사용하지 않아요</Caption2>
+                    </Terms>
 
-                <Terms onClick={() => setCheck(!check)} check={check}>
-                    <CheckCircle fill={check ? '#8787F4' : '#C1C4CC'} />
-                    <Caption2>제공받는 혜택이 부족해요</Caption2>
-                </Terms>
+                    <Terms onClick={() => handleOptionClick('option4')} check={selectedOptions['option4']}>
+                        <CheckCircle fill={selectedOptions['option4'] ? '#8787F4' : '#C1C4CC'} />
+                        <Caption2>제공받는 혜택이 부족해요</Caption2>
+                    </Terms>
 
-                <Terms onClick={() => setCheck(!check)} check={check}>
-                    <CheckCircle fill={check ? '#8787F4' : '#C1C4CC'} />
-                    <Caption2>전문가 등업이 너무 어려워요</Caption2>
-                </Terms>
+                    <Terms onClick={() => handleOptionClick('option5')} check={selectedOptions['option5']}>
+                        <CheckCircle fill={selectedOptions['option5'] ? '#8787F4' : '#C1C4CC'} />
+                        <Caption2>전문가 등업이 너무 어려워요</Caption2>
+                    </Terms>
 
-                <Terms onClick={() => setCheck(!check)} check={check}>
-                    <CheckCircle fill={check ? '#8787F4' : '#C1C4CC'} />
-                    <Caption2>다른 서비스를 이용하고 싶어요</Caption2>
-                </Terms>
+                    <Terms onClick={() => handleOptionClick('option6')} check={selectedOptions['option6']}>
+                        <CheckCircle fill={selectedOptions['option6'] ? '#8787F4' : '#C1C4CC'} />
+                        <Caption2>다른 서비스를 이용하고 싶어요</Caption2>
+                    </Terms>
+                </Options>
 
-            
+                <ButtonLay>
+                    <Button children="다음" disabled={!isButtonEnabled} onClick={openModal} />
+                </ButtonLay>
 
             </Text>
+            {isModalOpen && (
+                <Cover>
+                    <Modal text="탈퇴하시겠습니까?" btn="확인" onClick={closeModal} />
+                </Cover>
+            )}
         </Cover>
+        
         </>
     );
 };
@@ -97,3 +133,11 @@ const Terms = styled('div', {
     },
   });
   
+const Options = styled("div", {
+    marginTop: "19px",
+});
+
+const ButtonLay = styled("div", {
+    marginTop: "49px",
+    marginLeft: "-5px",
+});
