5210b015d3a332f537bb2412cf24e5f23147a1dc
alreadynyeong <kjw87130@naver.com>
2023-11-06 22:50:13 +0900

    #54 Design: 이미지 업로드 수정

--- iTER/src/component/review/ImageUpload.tsx
+++ iTER/src/component/review/ImageUpload.tsx
@@ -2,6 +2,7 @@ import React, { useState, ChangeEvent, useRef } from 'react';
 import { styled } from '../../../stitches.config';
 import Xbtn from '../../assets/icon/Xbtn.svg?react';
 import Plus from '../../assets/icon/Plus.svg?react';
+import { Caption1 } from '../Font';
 
 interface ImageUploadProps {
   onImageSelected: (image: File) => void;
@@ -32,7 +33,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onImageSelected }) => {
   };
 
   return (
-    <div>
+    <Container>
       <input
         type="file"
         accept="image/*"
@@ -40,66 +41,93 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onImageSelected }) => {
         ref={fileInputRef}
         style={{ display: 'none' }}
       />
-      <div
-        onClick={handleImagePreviewClick}
-        style={{ cursor: 'pointer', position: 'relative' }}
-      >
+      <div onClick={handleImagePreviewClick} style={{ cursor: 'pointer', position: 'relative' }}>
         <ImageGallery>
+          <Pluscover>
+            <Cover>
+              <Plus width="24px" height="24px" />
+            </Cover>
+            <Caption1>
+              <Count>(1/5)</Count>
+            </Caption1>
+          </Pluscover>
           {selectedImages.map((image, index) => (
             <ImageContainer key={index}>
               <XbtnContainer onClick={() => handleImageDelete(index)}>
                 <Xbtn />
               </XbtnContainer>
-              <img
-                src={URL.createObjectURL(image)}
-                alt="Selected"
-                width={100}
-              />
+              <img src={URL.createObjectURL(image)} alt="Selected" width={100} height={100} />
             </ImageContainer>
           ))}
-          <Pluscover>
-            <Cover>
-                <Plus width="24px" height="24px" />
-            </Cover>
-          </Pluscover>
         </ImageGallery>
       </div>
-    </div>
+    </Container>
   );
 };
 
 export default ImageUpload;
 
-const ImageGallery = styled("div", {
-  display: "flex",
-  flexDirection: "row",
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  overflow: 'hidden',
 });
 
-const ImageContainer = styled("div", {
-  width: "120px",
-  height: "120px",
-  backgroundColor: "#EAEEF2",
-  margin: "0 10px", 
+const ImageGallery = styled('div', {
+  display: 'inline-flex',
+  height: '100px',
+  width: '100%',
+  overflowX: 'scroll',
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+});
+
+const ImageContainer = styled('div', {
+  width: '100px',
+  minWidth: '100px',
+  height: '100px',
+  backgroundColor: '#EAEEF2',
+  margin: '0 10px',
   position: 'relative',
-  textAlign: "center",
-  alignItems: "center",
+  textAlign: 'center',
+  alignItems: 'center',
+  overflow: 'hidden',
 });
 
-const XbtnContainer = styled("div", {
-  position: "absolute",
-  top: "0",
-  right: "0",
-  cursor: "pointer",
+const XbtnContainer = styled('div', {
+  position: 'absolute',
+  top: '0',
+  right: '0',
+  cursor: 'pointer',
+  width: '14px',
+  height: '14px',
+  borderRadius: '50%',
+  backgroundColor: '#D9D9D9',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '4px',
 });
 
-const Pluscover = styled("div", {
-  marginBottom: "-20px",
-  width: "120px",
-  height: "120px",
-  backgroundColor: "#EAEEF2",
+const Pluscover = styled('div', {
+  marginBottom: '-20px',
+  width: '100px',
+  minWidth: '100px',
+  height: '100px',
+  backgroundColor: '#EAEEF2',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 });
 
-const Cover = styled("div", {
-    textAlign: "center",
-    marginTop: "45px",
-})
+const Cover = styled('div', {
+  textAlign: 'center',
+  marginTop: '29px',
+  height: '24px',
+});
+
+const Count = styled('span', {
+  color: '#AFB8C1',
+});
