37345eefb1239f2123d1188360086730cea6a3d0
kimtaeyoung <trust00005@naver.com>
2023-11-20 12:12:21 +0900

    feat 헤더에 토큰 값 저장하는 방식 변경

--- iTER/src/pages/Login.tsx
+++ iTER/src/pages/Login.tsx
@@ -48,8 +48,9 @@ const Login = () => {
             localStorage.setItem('expiredTime', expiredTime);
 
             //저장한 토큰 값들 헤더에 저장(백에서 유효성 검사를 통해 엑세스와 리프레시 재발급 해줌)
-            //헤더에 두개 저장 이렇게 하는게 맞나..
-            axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken},${refreshToken}`;
+            //각각의 헤더에 저장
+            axios.defaults.headers.common['Authorization'] = accessToken;
+            axios.defaults.headers.common['Authorization-refresh'] = refreshToken;
             navigate('/');
 
         }
