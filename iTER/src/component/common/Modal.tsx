409a8204552e8cbbe7bdf5196c23acf6ab39c28d
alreadynyeong <kjw87130@naver.com>
2023-11-24 21:27:05 +0900

    #81 Fix: 모달창 세로정렬 수정

--- iTER/src/component/common/Modal.tsx
+++ iTER/src/component/common/Modal.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { styled } from '../../../stitches.config';
 import { ButtonText, Headline4 } from '../Font';
-import Xbtn from "../../assets/icon/Xbtn.svg?react";
+import Xbtn from '../../assets/icon/Xbtn.svg?react';
 
 interface ModalProps {
   text: string;
@@ -59,18 +59,18 @@ export const ModalSelect: FC<ModalProps> = ({ text, btn, onClick, onClosed }) =>
 
 export const ModalMyPoint: FC<ModalProps> = ({ onClick, onClosed }) => {
   const score = [
-    { title: "리뷰를 작성하면" , score: "+ 20점" },
-    { title: "IT 퀴즈를 풀면" , score: "+3점" },
-    { title: "IT 퀴즈 정답을 맞히면" , score: "+5점" },
-    { title: "리뷰 좋아요" , score: "+7점"},
-    { title: "리뷰 스크랩" , score: "+10점"},
+    { title: '리뷰를 작성하면', score: '+ 20점' },
+    { title: 'IT 퀴즈를 풀면', score: '+3점' },
+    { title: 'IT 퀴즈 정답을 맞히면', score: '+5점' },
+    { title: '리뷰 좋아요', score: '+7점' },
+    { title: '리뷰 스크랩', score: '+10점' },
   ];
 
   return (
     <Back
-    onClick={() => {
-      onClosed && onClosed();
-    }}
+      onClick={() => {
+        onClosed && onClosed();
+      }}
     >
       <Container>
         <PointModalBox>
@@ -78,27 +78,27 @@ export const ModalMyPoint: FC<ModalProps> = ({ onClick, onClosed }) => {
             <Xbtn width={28} height={28} />
           </XbtnLay>
           <Title>
-            <Headline4>포인트가 500점 이상이면<br />ITet가 인정하는 IT 전문가에요</Headline4>
+            <Headline4>
+              포인트가 500점 이상이면
+              <br />
+              ITet가 인정하는 IT 전문가에요
+            </Headline4>
           </Title>
           <Info>
             {score.map((item, index) => (
-                  <Item key={index}>
-                      <ContainerP>
-                          <TitleContainer>
-                              {item.title}
-                          </TitleContainer>
-                          <ScoreContainer>
-                              {item.score}
-                          </ScoreContainer>
-                      </ContainerP>
-                  </Item>
-              ))}
+              <Item key={index}>
+                <ContainerP>
+                  <TitleContainer>{item.title}</TitleContainer>
+                  <ScoreContainer>{item.score}</ScoreContainer>
+                </ContainerP>
+              </Item>
+            ))}
           </Info>
         </PointModalBox>
       </Container>
     </Back>
-  )
-}
+  );
+};
 
 export default Modal;
 
@@ -124,7 +124,6 @@ const Container = styled('div', {
 });
 
 const ModalBox = styled('div', {
-  marginTop: '50%',
   width: '340px',
   backgroundColor: `$White`,
   borderRadius: '20px',
@@ -137,6 +136,9 @@ const ModalBox = styled('div', {
   },
   overflow: 'hidden',
   pointerEvents: 'auto',
+  position: 'fixed',
+  transform: 'translateY(-50%)',
+  top: '50%',
 });
 
 const Button = styled('div', {
@@ -166,49 +168,51 @@ const SelectButton = styled('div', {
   },
 });
 
-const PointModalBox = styled("div", {
-  width: "340px",
-  height: "296px",
-  borderRadius: "20px",
-  backgroundColor: "#FFF",
-  marginTop: "50%",
-})
+const PointModalBox = styled('div', {
+  width: '340px',
+  height: '296px',
+  borderRadius: '20px',
+  backgroundColor: '#FFF',
+  position: 'fixed',
+  transform: 'translateY(-50%)',
+  top: '50%',
+});
 
-const Title = styled("div", {
-  color: "24292F",
-  textAlign: "center",
-  marginTop: "-10px",
-})
+const Title = styled('div', {
+  color: '24292F',
+  textAlign: 'center',
+  marginTop: '-10px',
+});
 
 //여기서부터 point 모달창 스타일
-const ContainerP = styled("div", {
-  width: "250px",
-  marginLeft: "46px",
+const ContainerP = styled('div', {
+  width: '250px',
+  marginLeft: '46px',
 });
 
-const Item = styled("div", {
+const Item = styled('div', {
   bodyText: 1,
-  display: "flex",
-  marginBottom: "8px",
-  color: "#8E9198",
+  display: 'flex',
+  marginBottom: '8px',
+  color: '#8E9198',
 });
 
-const TitleContainer = styled("div", {
-  float: "left",
-})
+const TitleContainer = styled('div', {
+  float: 'left',
+});
 
-const ScoreContainer = styled("div", {
-  float: "right",
-})
+const ScoreContainer = styled('div', {
+  float: 'right',
+});
 
-const Info = styled("div", {
-  marginTop: "30px",
-})
+const Info = styled('div', {
+  marginTop: '30px',
+});
 
-const XbtnLay = styled("div", {
-  display: "flex",
-  justifyContent: "flex-end",  
-  marginRight: "16px",  
-  marginTop: "16px",
-  cursor: "pointer",  
-})
+const XbtnLay = styled('div', {
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginRight: '16px',
+  marginTop: '16px',
+  cursor: 'pointer',
+});
