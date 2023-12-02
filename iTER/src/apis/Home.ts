c98b0274a54a92342cda4ae80ef6bbde6d67cf8b
alreadynyeong <kjw87130@naver.com>
2023-12-02 12:17:40 +0900

    #79 Feat: 헤더 index로 통일

--- iTER/src/apis/Home.ts
+++ iTER/src/apis/Home.ts
@@ -1,17 +1,10 @@
-import axios from 'axios';
 import api from './index';
 
 // 홈 데이터 조회
 export const getHome = async () => {
   console.log('홈 데이터 조회');
   try {
-    const accessToken = localStorage.getItem('accessToken');
-    axios.defaults.headers.common['Authorization'] = `${accessToken}`;
-    const response = await api.get('/home', {
-      headers: {
-        Authorization: `${accessToken}`,
-      },
-    });
+    const response = await api.get('/home');
     return response.data.result;
   } catch (error) {
     console.log('홈 데이터 조회 오류', error);
