496b88737828dfbfbd2c5e2d91b2d542a80456e9
alreadynyeong <kjw87130@naver.com>
2023-12-03 15:42:42 +0900

    #96 Fix: 토큰 수정

--- iTER/src/apis/Common.ts
+++ iTER/src/apis/Common.ts
@@ -2,9 +2,14 @@ import api from './index';
 
 // 카테고리 데이터 조회
 export const getCategory = async () => {
+  const accessToken = localStorage.getItem('accessToken');
   console.log('카테고리 데이터 조회');
   try {
-    const response = await api.get('/category');
+    const response = await api.get('/category', {
+      headers: {
+        Authorization: accessToken ? `${accessToken}` : '',
+      },
+    });
     return response.data.result;
   } catch (error) {
     console.log('카테고리 데이터 조회 오류', error);
