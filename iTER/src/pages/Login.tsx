0e3fe6b4f45ceb1d52e7afd8c00e089a7f1fa389
alreadynyeong <kjw87130@naver.com>
2024-01-07 17:24:42 +0900

    #110 Fix: 회원가입 데이터 수정

--- iTER/src/pages/Login.tsx
+++ iTER/src/pages/Login.tsx
@@ -4,15 +4,14 @@ import LoginInput from '../component/common/LoginInput';
 import Button from '../component/common/Button';
 import Kakao from '../assets/icon/Kakao.svg?react';
 import { useNavigate } from 'react-router-dom';
-import axios from "axios";
+import axios from 'axios';
 import { Caption1, Headline3 } from '../component/Font';
 import LoginErrorIcon from '../assets/icon/LoginErrorIcon.svg?react';
 import { postLogin } from '../apis/login';
 
 const Login = () => {
-
   const navigate = useNavigate();
- 
+
   const [emailValue, setEmailValue] = useState<string>(''); // 이메일 입력 값
   const [passwordValue, setPasswordValue] = useState<string>(''); // 비밀번호 입력 값
   const [errorMessage, setErrorMessage] = useState<string>('');
@@ -26,13 +25,13 @@ const Login = () => {
     setPasswordValue(newValue);
   };
 
-  const handleLogin = async (email: string, password: string) => { 
+  const handleLogin = async (email: string, password: string) => {
     try {
       setLoginState(false);
       const loginData = await postLogin(email, password);
 
       //로그인 성공시 토큰 값들과 만료기한 저장하기
-      const {accessToken, refreshToken, expiredTime} = loginData.result;
+      const { accessToken, refreshToken, expiredTime } = loginData.result;
 
       //저장한 값들 로컬스토리지에 넣어주기
       localStorage.setItem('accessToken', accessToken);
@@ -46,39 +45,32 @@ const Login = () => {
       console.log('Headers:', axios.defaults.headers.common);
 
       navigate('/');
-    }
-    catch(error) {
+    } catch (error) {
       setLoginState(true);
       const errorData = error.response.data.code;
       const errorMessage = error.response.data.message;
 
-      console.log("로그인 에러", errorData);
-      if (errorData == "USER_NOT_FOUND_400") {
+      console.log('로그인 에러', errorData);
+      if (errorData == 'USER_NOT_FOUND_400') {
         console.log(errorData);
         setErrorMessage(errorMessage);
-      }
-      else if (errorData == "AUTH_PASSWORD_NOT_MATCH_401") {
+      } else if (errorData == 'AUTH_PASSWORD_NOT_MATCH_401') {
         console.log(errorData);
         setErrorMessage(errorMessage);
-      }
-      else if (errorData == "METHOD_ARGUMENT_ERROR") {
+      } else if (errorData == 'METHOD_ARGUMENT_ERROR') {
         console.log(errorData);
-        setErrorMessage("이메일 또는 비밀번호가 올바른 형식이 아닙니다.");
-      }
-      else if (errorData == "AUTH_SHOULD_BE_KAKAO_401") {
+        setErrorMessage('이메일 또는 비밀번호가 올바른 형식이 아닙니다.');
+      } else if (errorData == 'AUTH_SHOULD_BE_KAKAO_401') {
         console.log(errorData);
-        setErrorMessage("카카오로 로그인한 이메일입니다");
-      }
-      else {
+        setErrorMessage('카카오로 로그인한 이메일입니다');
+      } else {
         console.log(errorData);
       }
-      
     }
   };
 
   // 입력 필드가 비어있지 않은 경우에만 버튼 활성화
   const isButtonEnabled = emailValue.trim() !== '' && passwordValue.trim() !== '';
-
 
   return (
     <>
@@ -100,21 +92,23 @@ const Login = () => {
             onChange={handlePasswordChange}
           />
         </Password>
-        
-          <Error>
-         
-            {loginState && (
-              <Icon>
-                <LoginErrorIcon />
-              </Icon>
-            )}
-          
-            
-            <Caption1>{errorMessage}</Caption1>
-          </Error>
-        
+
+        <Error>
+          {loginState && (
+            <Icon>
+              <LoginErrorIcon />
+            </Icon>
+          )}
+
+          <Caption1>{errorMessage}</Caption1>
+        </Error>
+
         <BtnBody>
-          <Button children="로그인" disabled={!isButtonEnabled} onClick={() => handleLogin(emailValue, passwordValue)} />
+          <Button
+            children="로그인"
+            disabled={!isButtonEnabled}
+            onClick={() => handleLogin(emailValue, passwordValue)}
+          />
         </BtnBody>
         <KakaoButton>
           <In>
@@ -124,7 +118,13 @@ const Login = () => {
         </KakaoButton>
 
         <Footer>
-          <Join onClick={() => console.log('회원가입으로 이동')}>회원가입</Join>
+          <Join
+            onClick={() => {
+              navigate('/signup');
+            }}
+          >
+            회원가입
+          </Join>
           <Line> | </Line>
           <Find onClick={() => console.log('비밀번호 찾기로 이동')}>비밀번호 찾기</Find>
         </Footer>
@@ -189,16 +189,15 @@ const Title = styled('div', {
   marginTop: '170px',
   marginLeft: '25px',
 });
-const Error = styled("div", {
-  color: "$ErrorRed",
-  display: "flex",
-  marginTop: "10px",
-  float: "left",
-  width: "100%",
-  marginLeft: "50px",
+const Error = styled('div', {
+  color: '$ErrorRed',
+  display: 'flex',
+  marginTop: '10px',
+  float: 'left',
+  width: '100%',
+  marginLeft: '50px',
 });
 
-const Icon = styled("div", {
-  paddingRight: "4px",
-})
-
+const Icon = styled('div', {
+  paddingRight: '4px',
+});
