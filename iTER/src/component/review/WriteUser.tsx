20b9174463724fb32fe928ef4d6fac7d7930bed7
alreadynyeong <kjw87130@naver.com>
2024-01-18 21:53:25 +0900

    #150 Fix: 유저 프로필 없는 경우 처리

--- iTER/src/component/review/WriteUser.tsx
+++ iTER/src/component/review/WriteUser.tsx
@@ -1,40 +1,51 @@
-import React from "react";
-import { styled } from "../../../stitches.config";
-import { B1 } from "../Font";
+import React from 'react';
+import { styled } from '../../../stitches.config';
+import { B1 } from '../Font';
+import UserIcon from '../../assets/icon/User.svg?react';
 
 interface UserProps {
-    img: string;
-    name: string;
-    job: string;
+  img: string;
+  name: string;
+  job: string;
 }
-const WriteUser: React.FC<UserProps> = ({img, name, job }) => {
-    return (
-        <>
-        <Cover>
-            <img src={img} alt="userImage" style={{maxWidth: "40px"}}/>
-            <Name><B1>{name}</B1></Name>
-            <Line><B1>|</B1></Line>
-            <Job><B1>{job}</B1></Job>
-        </Cover>
-        </>
-    );
+const WriteUser: React.FC<UserProps> = ({ img, name, job }) => {
+  return (
+    <>
+      <Cover>
+        {img ? (
+          <img src={img} alt={name} width={20} height={20} style={{ marginRight: 4 }} />
+        ) : (
+          <UserIcon width={20} height={20} style={{ marginRight: 4 }} />
+        )}
+        <Name>
+          <B1>{name}</B1>
+        </Name>
+        <Line>
+          <B1>|</B1>
+        </Line>
+        <Job>
+          <B1>{job}</B1>
+        </Job>
+      </Cover>
+    </>
+  );
 };
 
 export default WriteUser;
 
-const Name = styled("div", {
-    marginLeft: "8px",
-})
+const Name = styled('div', {
+  marginLeft: '8px',
+});
 
-const Job = styled("div", {
-    marginLeft: "2px",
-})
+const Job = styled('div', {
+  marginLeft: '2px',
+});
 
-const Line = styled("div", {
-    marginLeft: "2px",
-})
+const Line = styled('div', {
+  marginLeft: '2px',
+});
 
-const Cover = styled("div", {
-    display: "flex",
-    alignItems: "center",
-})
+const Cover = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+});
