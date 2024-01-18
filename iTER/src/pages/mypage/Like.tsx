ac57044847e2bfeee868255d0fc3520250083684
alreadynyeong <kjw87130@naver.com>
2024-01-18 23:12:03 +0900

    #150 Fix: 좋아요한 리뷰 리스트 수정

--- iTER/src/pages/mypage/Like.tsx
+++ iTER/src/pages/mypage/Like.tsx
@@ -1,6 +1,6 @@
 import { styled } from '../../../stitches.config';
 import Top from '../../component/layout/Top';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { getMypageReviewLike } from '../../apis/Mypage';
 import LoadingPage from '../../component/common/Loading';
 import ErrorPage from '../../component/common/Error';
@@ -8,39 +8,48 @@ import { CategoryReviewProps } from '../../types/Review';
 import ListItem from '../../component/search/ListItem';
 
 const Like = () => {
-  // const [page, setPage] = useState<number>(0);
   const page = 0;
 
   const {
     data: likeData,
     isLoading: likeLoading,
     isError: likeError,
-  } = useQuery<CategoryReviewProps>(['like', page], () => getMypageReviewLike(page));
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<CategoryReviewProps, Error>(
+    ['likedReview'],
+    ({ pageParam = 0 }) => getMypageReviewLike(pageParam),
+    {
+      getNextPageParam: (lastPage) => (lastPage.hasNext ? page + 1 : undefined),
+    }
+  );
 
   likeLoading && <LoadingPage />;
   likeError && <ErrorPage type={2} />;
 
-  console.log(likeData, 'likeData');
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { currentTarget } = e;
+    if (currentTarget instanceof HTMLDivElement) {
+      const { scrollHeight, scrollTop, clientHeight } = currentTarget;
+      // 스크롤이 리스크의 끝에 도달했을 때
+      if (scrollHeight - scrollTop === clientHeight) {
+        fetchNextPage();
+      }
+    }
+  };
+
   return (
     <Container>
       <Top title="좋아요한 리뷰" />
 
-      {likeData !== undefined && likeData.reviews.length > 0 ? (
-        <List>
-          {likeData.reviews.map((item, index) => (
-            <ListItem
-              key={index}
-              id={item.id}
-              productName={item.productName}
-              reviewSpecData={item.reviewSpecData}
-              starPoint={item.starPoint}
-              shortReview={item.shortReview}
-              userInfo={item.userInfo}
-              scrapedCount={item.scrapedCount}
-              likedCount={item.likedCount}
-              reviewImage={item.reviewImage}
-            />
-          ))}
+      {likeData !== undefined && likeData?.pages[0]?.existed ? (
+        <List onScroll={handleScroll}>
+          {likeData.pages.map((page, pageIndex) =>
+            page.reviews.map((item, index) => (
+              <ListItem key={`${pageIndex}-${index}`} item={item} />
+            ))
+          )}
+          {hasNextPage && <LoadingPage />} {/* 추가 데이터를 로드 중임을 표시 */}
         </List>
       ) : (
         <Empty>마음에 드는 리뷰에 좋아요를 눌러보세요</Empty>
